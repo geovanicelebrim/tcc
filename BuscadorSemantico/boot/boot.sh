cbf1524b1277e0432dede520ed1f136e8724ace6
Geovani Celebrim <geovanicelebrim@gmail.com>
2016-12-20 00:13:06 -0200

    Implementação do script de inicialização dos diretórios do sistema.

---
+++ BuscadorSemantico/boot/boot.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# initialise the search engine system
+#
+# Author:   Geovani Celebrim
+# Version:  2016-12-19
+
+#############################################
+DIR=`dirname $0`
+user=$USERNAME
+path="/home/$user/WebServer"
+
+if [ -d $path ]; then
+	echo "The path: $path already exist!"
+	echo "Please check it and try again."
+	exit
+fi
+#############################################
+
+#############################################
+ann="$path/repository/ann"
+data="$path/repository/data"
+dictionary="$path/repository/dictionary"
+meta="$path/repository/meta"
+
+mkdir -p $ann
+mkdir -p $data
+mkdir -p $dictionary
+mkdir -p $meta
+#############################################
+
+#############################################
+cp "$DIR/stopWordsPt.txt" $dictionary
+cp "$DIR/rest_query_template.py" $path
+#############################################
